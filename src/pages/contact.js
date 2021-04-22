e2cb761a146bf18ad8267f729329b9239b387c71
Meghan Hannon <megfhannon@gmail.com>
2021-04-22 19:51:15 -0400

    styled contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,52 +1,84 @@
 import React from 'react'; 
 import Layout from '../components/layout/Layout';
-import { Box, Input, Textarea as ChakraTextarea, Button, Heading } from "@chakra-ui/react"
+import { 
+  Box, 
+  Heading,
+  Input, Textarea as ChakraTextArea,
+  FormControl as ChakraFormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Button
+} from "@chakra-ui/react"
 import { graphql } from 'gatsby';
 import { FormiumForm, defaultComponents } from '@formium/react';
 import { formium } from '../lib/formium';
 
-// function TextInput(props) {
-//   return <Input 
-//           placeholder={props.placeholder} 
-//           size="md" 
-//           variant="outline"
-//           focusBorderColor="purple.800"
-//         />
-// }
 
-// function Textarea(props) {
-//   return <ChakraTextarea focusBorderColor="purple.800"></ChakraTextarea>
-// }
+function FormControl(props) {
+  return (
+      <ChakraFormControl id={props.id} marginTop="2">
+        <FormLabel color="purple.800">{props.label}</FormLabel>
+        <FormHelperText>{props.description}</FormHelperText>
+          {props.children} 
+      </ChakraFormControl>
+  )
+}
 
-// function SubmitButton(props) {
-//   return <Button colorScheme="purple" {...props} >Submit</Button>
-// }
+function TextInput(props){
+  return (
+    <FormControl>
+      <Input {...props} placeholder={props.placeholder} required={props.required} focusBorderColor="purple.600"/>
+      <FormErrorMessage></FormErrorMessage>
+    </FormControl>   
+  )    
+}
 
-// function Header(props){
-//     const { page: {title} } = props
-    
-//     return (
-//         <Heading as="h4" marginTop="4" marginBottom="4" size="lg" colorScheme="purple">{title}</Heading>
-//     )
-// }
+function Textarea(props){
+  return (
+    <FormControl>
+      <ChakraTextArea {...props} variant="outline" focusBorderColor="purple.600" required={props.required}/>
+    </FormControl>
+  )
+}
 
-// const myComponents = { ...defaultComponents, TextInput, Textarea, SubmitButton, Header }; 
+function Header(props){
+  const { page: {title} } = props; 
+  return (
+    <Heading as="h4" marginTop="4" marginBottom="4" color="purple.800">{title}</Heading>
+  )
+}
 
-const myComponents = { ...defaultComponents }; 
+function SubmitButton(props){
+  return(
+    <Button colorScheme="purple" size="md" {...props} mt="2"/>
+  )
+}
+
+// Always define these outside of React so that
+// components are not needlessly recreated on each render
+const myComponents = {
+    ...defaultComponents,
+    TextInput,
+    Header,
+    FormControl,
+    Textarea,
+    SubmitButton
+}
 
 export default function Contact({ data }) {
   const [success, setSuccess] = React.useState(false);
   if (success) {
     return (
       <Layout>
-        <Box>Thank you! Your response has been recorded.</Box>
+        <Heading size="md" color="purple.800">Thank you! Your response has been recorded.</Heading>
       </Layout>
     );
   }
 
   return (
     <Layout>
-      <Box>
+      <Box w={400}>
         <FormiumForm
           data={data.formiumForm}
           components={myComponents}
